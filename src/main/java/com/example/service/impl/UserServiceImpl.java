5382dcacfcc6974d1fc0b1cf696efa84c9b2e16e
Freedom-R <1144188685@qq.com>
2020-05-28 23:43:20 +0800

    UserService Version:1

---
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.service.impl;
+
+/**
+ * @Project: management_system
+ * @Package: com.example.service.impl
+ * @Author: 周博义
+ * @Date: Created in 2020/5/28 23:42
+ */
+public class UserServiceImpl {
+}
