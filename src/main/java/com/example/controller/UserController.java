910ac1ae20012834dfe487c5df38d3919b969ddd
Freedom-R <1144188685@qq.com>
2020-05-28 23:19:47 +0800

    UserController Version:1

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+/**
+ * @Project: management_system
+ * @Package: com.example.controller
+ * @Author: 周博义
+ * @Date: Created in 2020/5/28 23:14
+ */
+
+public class UserController {
+}
