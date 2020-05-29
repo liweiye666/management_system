233eb6687767b30a8b232bf6bb7509a05b714d40
liweiye666 <1320268524@qq.com>
2020-05-29 16:47:12 +0800

    修改PageController和增加了html页面

--- src/main/java/com/example/controller/PageController.java
+++ src/main/java/com/example/controller/PageController.java
@@ -1,10 +1,40 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Project: management_system
  * @Package: com.example.controller
  * @Author: 周博义
  * @Date: Created in 2020/5/29 11:25
  */
+
+@Controller
+@RequestMapping("/page")
 public class PageController {
+
+    //用户管理界面跳转
+    @RequestMapping("/toUser")
+    public String toUser(){
+        return "menu/userdb.html";
+    }
+
+    //菜单管理界面跳转
+    @RequestMapping("/toMenu")
+    public String toMenu(){
+        return "menu/menudb.html";
+    }
+
+    //角色管理界面跳转
+    @RequestMapping("/toRole")
+    public String toRole(){
+        return "menu/roledb.html";
+    }
+
+    //部门管理界面跳转
+    @RequestMapping("/toDept")
+    public String toDept(){
+        return "menu/deptdb.html";
+    }
 }
