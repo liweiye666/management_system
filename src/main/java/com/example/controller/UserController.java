18a266e02ecc647959eb021b351c077b6c88c38b
liweiye666 <1320268524@qq.com>
2020-05-29 15:18:09 +0800

    修改UserController

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Project: management_system
  * @Package: com.example.controller
@@ -7,5 +10,11 @@ package com.example.controller;
  * @Date: Created in 2020/5/28 23:14
  */
 
+@Controller
 public class UserController {
+
+    @RequestMapping("/tolgoin")
+    public String toLogin(){
+        return "login.html";
+    }
 }
